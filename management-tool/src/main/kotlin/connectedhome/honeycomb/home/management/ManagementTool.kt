0186c5194a67f9f06cf9eb9b18125abe8f5e826f
Tim Clark <tim.clark@hivehome.com>
2017-04-10 15:54:20 +0100

    Modify form components

--- management-tool/src/main/kotlin/connectedhome/honeycomb/home/management/ManagementTool.kt
+++ management-tool/src/main/kotlin/connectedhome/honeycomb/home/management/ManagementTool.kt
@@ -6,12 +6,30 @@ import spark.Spark.*
 
 fun main(args: Array<String>) {
 	staticFiles.location("/public")
-	get("/ping") { req, rsp -> "PONG!" }
+	get("/ping") { _, _ -> "PONG!" }
 	get("/home") { _, _ -> render(emptyMap<String, Any>(), "overview") }
-	get("/results") { _, _ -> render(emptyMap<String, Any>(), "results") }
+	get("/results") { _, _ -> render(mapOf("accounts" to results()), "results") }
 }
 
 private fun render(model: Map<String, Any>, template: String): String =
 	HandlebarsTemplateEngine().render(ModelAndView(model, template))
+
+
+enum class AccountStatus {
+	Active, Suspended, Closed
+}
+data class AccountOverview(val id: String, val owner: String, val status: AccountStatus, val properties: Int, val users: Int)	{
+	val active = status == AccountStatus.Active
+	val suspended = status == AccountStatus.Suspended
+	val closed = status == AccountStatus.Closed
+}
+
+private fun results(): List<AccountOverview> {
+	return listOf(
+		AccountOverview("1", "Tommy Tippee", AccountStatus.Active, 1, 7),
+		AccountOverview("2", "Zippy Zappee", AccountStatus.Suspended, 2, 5),
+		AccountOverview("3", "Big Bertrand", AccountStatus.Closed, 1, 8)
+	)
+}
 
 
